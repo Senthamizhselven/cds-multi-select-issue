1a907628d7902fcb7c44193f2ee2d78e1084dc90
Senthamil Selven <ssenthamil@vmware.com>
2023-07-12 18:41:41 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'cds-multi-select-issue';
+export class AppComponent implements OnInit {
+  errorForm?: FormGroup;
+  options = [
+    'India',
+    'United States',
+    'United Kingdom',
+    'South Africa',
+    'Brazil',
+  ];
+
+  ngOnInit(): void {
+    this.errorForm = new FormGroup({
+      multiSelect: new FormControl(),
+      name: new FormControl()
+    });
+  }
+
+  onSubmit = ()=>{
+    alert("Selected countries: " + this.errorForm?.value.multiSelect)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CdsModule } from '@cds/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CdsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
